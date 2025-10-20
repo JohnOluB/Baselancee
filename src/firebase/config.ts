933f730ccc0a2554d3e8c638c35b575bcbd884c6
ADEBAYO John <adebayodeolu@gmail.com>
2025-10-20 19:31:17 +0000

    update the config file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,10 +6,12 @@ const firebaseConfig: FirebaseOptions = {
   "appId": "1:95140804609:web:d9d886c175cee5f24ba800",
   "apiKey": "AIzaSyBcHHWRoZrLgP-WXNoiIsIF9HO4XdjLXIo",
   "authDomain": "studio-27276494-d8631.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-9T4C1JCF5W",
+  "storageBucket": "studio-27276494-d8631.appspot.com",
   "messagingSenderId": "95140804609"
 };
 
+// Initialize Firebase
 function initializeFirebase() {
     if (!getApps().length) {
         return initializeApp(firebaseConfig);
