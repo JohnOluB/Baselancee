c731e4fdf6c037015add349cf610f88283621cf5
ADEBAYO John <adebayodeolu@gmail.com>
2025-10-22 14:07:27 +0000

    change the background of the button to white when the cursor is on it (_

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -57,7 +57,7 @@ export default function Header() {
 
         <div className="hidden items-center gap-2 md:flex">
           <Button variant="ghost" asChild>
-            <Link href="/login" className="transition-colors font-medium text-sm text-foreground">
+            <Link href="/login" className="transition-colors font-medium text-sm text-foreground hover:bg-white">
               Log In
             </Link>
           </Button>
