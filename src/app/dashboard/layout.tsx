5e501469dabeb7b635d1cd0cf9f40556ad478041
ADEBAYO John <adebayodeolu@gmail.com>
2025-10-22 21:31:16 +0000

    margin left 12px (_for element div_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,7 +14,7 @@ export default function DashboardLayout({
         <DashboardSidebar />
         <main className="flex-1 flex flex-col bg-muted/30">
           <DashboardHeader />
-          <div className="flex-1 py-4 sm:py-6 md:py-8">
+          <div className="flex-1 py-4 sm:py-6 md:py-8 ml-3">
             <div className="container mx-auto">
               {children}
             </div>
