296b555cc4382ff268348844479dd9617a89c1c5
ADEBAYO John <adebayodeolu@gmail.com>
2025-10-24 04:33:51 +0000

    Image Prompt: "Professional hero illustration for a freelancer marketpla

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.pravatar.cc',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
