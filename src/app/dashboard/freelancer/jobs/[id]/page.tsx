5f15381bdd9544acdd64b72c7e1341b6d8b0c723
ADEBAYO John <adebayodeolu@gmail.com>
2025-10-24 07:23:40 +0000

    when the input is active please erase all placeholder texts (_for elemen

--- src/app/dashboard/freelancer/jobs/[id]/page.tsx
+++ src/app/dashboard/freelancer/jobs/[id]/page.tsx
@@ -339,7 +339,7 @@ function ApplicationSidebar() {
                                         <Input 
                                             id="bid-amount" 
                                             type="number" 
-                                            className="pl-12"
+                                            className="pl-12 focus:placeholder-transparent"
                                             placeholder="Enter your bid..."
                                             {...register('bidAmount', { valueAsNumber: true })}
                                         />
@@ -670,6 +670,8 @@ export default function JobDetailsPage() {
     </div>
   );
 }
+
+    
 
     
 
