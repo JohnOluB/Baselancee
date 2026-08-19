0588c005b168e996307ce4a6c6669dd19a489866
JohnOluB <adebayodeolu90@gmail.com>
2026-08-19 08:27:45 +0100

    feat: integrate supabase

--- src/app/supabase-client.tsx
+++ src/app/supabase-client.tsx
@@ -1,6 +1,6 @@
-import { createClient } from "supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = process.env.SUPABASE_URL!
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
 
-const supabaseUrl = 
-
-export const 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
